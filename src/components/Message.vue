f69999950ea408662331af8f4a8f5f862534963a
dice <ds_work_prog@outlook.jp>
2022-01-10 22:05:35 +0900

    link

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="message">
-    <a v-if="!sender" href="https://twitter.com/{{sender}}"><span>{{ name }}</span></a>
-    <a v-else href="https://twitter.com/{{sender}}"><span  class="font_italic sender">{{ name }}</span></a>
+    <a v-if="!sender" :href="'https://twitter.com/'+sender"><span>{{ name }}</span></a>
+    <a v-else :href="'https://twitter.com/'+sender"><span  class="font_italic sender">{{ name }}</span></a>
     <div class="flex" :class="sender ? 'flex-row-reverse' : ''">
       <Avatar class="mt-1" :src="photoUrl" />
       <div class="text w-3/4" :class="sender ? 'bg-green-800' : 'bg-gray-700'">

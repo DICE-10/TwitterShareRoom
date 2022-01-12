1624b7874cd96c2a4228c6a9ab35ad07b3a333b2
dice <ds_work_prog@outlook.jp>
2022-01-12 21:58:28 +0900

    Chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -7,7 +7,7 @@
         :name="userName"
         :photo-url="userPhotoURL"
         :sender="userId === user?.uid"
-        :user="twitterId.value"
+        :user="twitterId"
       >
         <span class="text_white" v-html="text.replace(/\n/g,'<br/>')"></span>
       </Message>
